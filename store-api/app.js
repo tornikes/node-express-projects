8ba41b936b86019e67f4d99ffdf8ee70b3a4aa22
tornikes <t.sadghobelashvili@gmail.com>
2021-12-29 14:46:18 +0400

    feat: products router

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors?
+
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API <a href="/api/v1/products">Products</a></h1>');
+});
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+async function start() {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(port, () => console.log(`Server is listening, port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
