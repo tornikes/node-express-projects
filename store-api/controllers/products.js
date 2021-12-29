853055bee3e8cdef40a1413d2c235e59ce732c43
tornikes <t.sadghobelashvili@gmail.com>
2021-12-29 16:06:05 +0400

    feat: sorting and searching

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort("-name price");
 
   res.status(200).json(products);
 };
@@ -9,7 +9,7 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   console.log(req.query);
 
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -22,7 +22,26 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "ig" };
   }
 
-  const products = await Product.find(queryObject);
+  let results = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    results = results.sort(sortList);
+  } else {
+    results = results.sort("-createdAt");
+  }
+
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    results = results.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  results = results.skip(skip).limit(limit);
+
+  const products = await results;
 
   res.status(200).json({ products, nbHits: products.length });
 };
