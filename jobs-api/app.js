d525137c0b7d0f93ed15fdb9007b11d28e9572e0
tornikes <t.sadghobelashvili@gmail.com>
2021-12-31 14:18:42 +0400

    feat: add security packages

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -10,8 +16,18 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// enable when behind reverse proxy
+// app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes,
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 const connectDB = require("./db/connect");
 const authenticateUser = require("./middleware/authentication");
